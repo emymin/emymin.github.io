1ebd08df8439ddddf34c32dfab4a9418d3c0eefc
emymin <emmyminmon@gmail.com>
2021-10-23 14:29:44 +0200

    Flag poles, ground plane

--- webgl/js/sketches/flags.js
+++ webgl/js/sketches/flags.js
@@ -2,8 +2,9 @@ import * as THREE from '/libs/three/build/three.module.js';
 import { RGBELoader } from '/libs/three/examples/jsm/loaders/RGBELoader.js';
 import '/libs/jquery.js';
 import { OrbitControls } from '/libs/three/examples/jsm/controls/OrbitControls.js';
-import { EquirectangularReflectionMapping,PlaneGeometry,MeshStandardMaterial,Mesh,TextureLoader } from '../../../libs/three/build/three.module.js';
+import { EquirectangularReflectionMapping,PlaneGeometry,MeshStandardMaterial,Mesh,TextureLoader, Plane } from '../../../libs/three/build/three.module.js';
 import { GetCustomStandard } from '../CustomStandardShader.js';
+import { degToRad } from '/libs/three/src/math/MathUtils.js';
 
 const texloader = new TextureLoader();
 
@@ -23,6 +24,7 @@ function makeFlag(nationName,flagURL,index,scene,flags){
   const loadFunc = (flagURL.endsWith(".svg")) ? loadSVGTex : texloader.load;
 
   const callback = (texture)=>{
+
     const flagMat = GetCustomStandard({name:"waveFlag_"+nationName,customVert:`
     vec4 origin = modelMatrix * vec4(0,0,0,1);
     float offset = sin(dot(origin.xz,vec2(23233.435,21234.534))*654523.12)*53431.23;
@@ -30,13 +32,34 @@ function makeFlag(nationName,flagURL,index,scene,flags){
     `,uniforms:uniforms,customVertHeader:"uniform float time;"});
     flagMat.map = texture;
     flagMat.side=THREE.DoubleSide;
+
+    const xpos = (index%5)*3;
+    const zpos = (index/5)*-1;
+
     const ratio = texture.image.width/texture.image.height;
     const flagGeom = new PlaneGeometry(1*ratio,1,10*ratio,1);
     const flag = new THREE.Mesh(flagGeom,flagMat);
     flag.name=nationName;
-    flag.position.set((index%5)*3,0,(index/5)*-1);
+    flag.position.set(xpos,5,zpos);
+
     scene.add(flag);
     flags.push(flag);
+
+    const poleGeom = new THREE.CylinderGeometry(0.05,0.05,5.5,10);
+    const poleMat = new THREE.MeshStandardMaterial();
+    poleMat.metalness=0.5;
+    poleMat.roughness=0.5;
+    const pole = new THREE.Mesh(poleGeom,poleMat);
+    pole.position.set(xpos-ratio*0.5,2.75,zpos);
+    pole.parent = flag;
+    scene.add(pole);
+
+    const poleTopGeom = new THREE.SphereGeometry(0.075,8,8);
+    const poleTop = new THREE.Mesh(poleTopGeom,poleMat);
+    poleTop.position.set(xpos-ratio*0.5,5.5,zpos);
+    poleTop.parent=pole;
+    scene.add(poleTop);
+    
   }
 
   if(flagURL.endsWith(".svg")){
@@ -94,7 +117,7 @@ function main() {
   const canvas = document.querySelector('#canvas');
   const renderer = new THREE.WebGLRenderer({canvas});
   renderer.autoClearColor=false;
-  const camera = new THREE.PerspectiveCamera( 20, window.innerWidth / window.innerHeight, 1, 10000 );
+  const camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 10000 );
 
   const scene = new THREE.Scene();
   
@@ -110,7 +133,14 @@ function main() {
   camera.position.set(0,0,10);
   const controls = new OrbitControls(camera,renderer.domElement);
   controls.target.set(6,0,-2);
+  controls.maxPolarAngle = (Math.PI/2)*0.9;
   controls.update();
+
+  const planeGeom = new PlaneGeometry(100,100);
+  const planeMat = new MeshStandardMaterial();
+  const plane = new THREE.Mesh(planeGeom,planeMat);
+  plane.rotation.set(degToRad(-90),0,0);
+  scene.add(plane);
 
   const flagsMeshes = [];
   flagsFromJson(flagsMeshes,scene,"/webgl/assets/data/flags.json");
@@ -151,4 +181,8 @@ function main() {
   requestAnimationFrame(render);
     
 }
+const title = document.createElement("p");
+title.classList.add("lower_left");
+title.innerHTML = "Flags from <a href=https://www.nationstates.net/region=the_lands_of_the_great_oh>the Lands of the Great Oh</a>";
+document.body.appendChild(title);
 main();
